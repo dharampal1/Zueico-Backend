58940a0cea55caf7902744602629569bd5fb854b
Dharampal Singh <you@example.com>
2018-08-10 12:47:18 +0530

    adding https one

--- config/environments/development.js
+++ config/environments/development.js
@@ -18,5 +18,5 @@ module.exports = {
     token_ContractAddress :'0x6806a1fb780173323ad41902539e12214ed3d994',
     veting_ContractAddress: '0xfff8465c2fe60749de6ebdb87a8fe09c7106e133',
     airdrop_ContractAddress :'0xeddc650bcba054015810aa93077ef41878b8af3d',
-    http_provider:"http://13.126.36.117:8899"
+    http_provider:"https://zuenchain.net:8899"
 };
