35f68f821138f58b85531a45487991760886a07e
Dharampal Singh <you@example.com>
2018-08-18 12:42:13 +0530

    add check in token update

--- helpers/cron-job.js
+++ helpers/cron-job.js
@@ -909,10 +909,10 @@ module.exports = {
                if(result.args.releasedAirDropAddress === user.ethWalletAddress) {
 
                	BuyToken.update({
-               		
+               		 tokenUpdateStatus:1,
                      tokens:result.args.value.toNumber()/ 10**18
                	},{
-               		where: { user_id:user.id }
+               		where: { [Op.and]: [{ user_id:user.id },{ buyHash: result.args.transactionHash }] } 
                	})
                	.then(buto => {
                		console.log("airdrop update");
