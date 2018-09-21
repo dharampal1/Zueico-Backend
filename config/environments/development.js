3909ec71c12f183dd4a35d111d6f634677eb077d
Expert Developer <you@example.com>
2018-09-21 11:18:43 +0530

    update new contract

--- config/environments/development.js
+++ config/environments/development.js
@@ -17,7 +17,7 @@ module.exports = {
     refund_ContractAddress :'0xba0619b9c8e99b1748a3462f4cb05b6b243db3a2',
     sale_ContractAddress: '0x3164afeadb754210c077b723fb2c32106cf0df65',
     token_ContractAddress :'0x6806a1fb780173323ad41902539e12214ed3d994',
-    veting_ContractAddress: '0x84e480b9127316d6637019485cd74baf2330e2f2',
+    veting_ContractAddress: '0x957f304c6fe3e3b0b4c96a308b85709272af4978',
     airdrop_ContractAddress :'0x9c0a941196c7795cc9c42ce1b3fab5d1df4b6abd',
     http_provider:"https://zuenchain.net:8899"
 };
