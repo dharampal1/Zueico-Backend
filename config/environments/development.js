efad7190f0517bc2dd31f3f52b3637fdc070afc9
Expert Developer <you@example.com>
2018-09-24 17:45:14 +0530

    update contract

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,23 +1,23 @@
 module.exports = {
   PORT: (process.env.PORT || 4000),
-  DATABASE_URL: (process.env.DATABASE_URL || ''), 
+  DATABASE_URL: (process.env.DATABASE_URL || ''),
   SECRET: (process.env.SECRET || 'h3sqq%pb#dHh^XcU8&Uj8brVS_*$LGHW'),
   JWT_EXPIRATION: (process.env.JWT_EXPIRATION || 600),
   smtpConfig: {
-       // host: 'smtp.gmail.com',
-       // port: 465,
-       // secure: true, // use SSL
-       service:'gmail',
-       auth: {
-          user: 'test@shamlatech.com',
-          pass: 'technical@123'
-       }
-    },
-    gapi_url:'https://zuenchain.net:5000',
-    refund_ContractAddress :'0xba0619b9c8e99b1748a3462f4cb05b6b243db3a2',
-    sale_ContractAddress: '0x3164afeadb754210c077b723fb2c32106cf0df65',
-    token_ContractAddress :'0x6806a1fb780173323ad41902539e12214ed3d994',
-    veting_ContractAddress: '0x0bd34ad1154485407ef7d4f19b56a0e85bcabb74',
-    airdrop_ContractAddress :'0x9c0a941196c7795cc9c42ce1b3fab5d1df4b6abd',
-    http_provider:"https://zuenchain.net:8899"
+    // host: 'smtp.gmail.com',
+    // port: 465,
+    // secure: true, // use SSL
+    service: 'gmail',
+    auth: {
+      user: 'test@shamlatech.com',
+      pass: 'technical@123'
+    }
+  },
+  gapi_url: 'https://zuenchain.net:5000',
+  refund_ContractAddress: '0xba0619b9c8e99b1748a3462f4cb05b6b243db3a2',
+  sale_ContractAddress: '0x3164afeadb754210c077b723fb2c32106cf0df65',
+  token_ContractAddress: '0x6806a1fb780173323ad41902539e12214ed3d994',
+  veting_ContractAddress: '0x9819106fe88365a69448c57267ea5befb6ea0a53',
+  airdrop_ContractAddress: '0x9c0a941196c7795cc9c42ce1b3fab5d1df4b6abd',
+  http_provider: "https://zuenchain.net:8899"
 };
